c0b41d13fffcc136b242132672eb995f26e26e9a
yumartins <yuri@estudioflow.com.br>
2020-03-09 21:50:35 -0300

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,13 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'prettier',
+    'prettier/react',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
@@ -20,7 +23,43 @@ module.exports = {
   },
   plugins: [
     'react',
+    'prettier',
+    'react-hooks',
+    'eslint-plugin-import-helpers',
   ],
   rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': 0,
+    camelcase: ['error', {
+      properties: 'never',
+      ignoreDestructuring: true,
+    }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'always',
+      asyncArrow: 'always',
+    }],
+    'space-unary-ops': [2, {
+      words: true,
+      nonwords: false,
+      overrides: {
+        '!': true,
+        '!!': true,
+      }
+    }],
+    'import-helpers/order-imports': ['warn', {
+        newlinesBetween: 'always',
+        groups: [
+          '/^react/',
+          'module',
+          '/^@shared/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
   },
 };
