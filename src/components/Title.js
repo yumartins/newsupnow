ae0105751fbf191dc460737a8888ce8efb5ab6dd
yumartins <yuri@estudioflow.com.br>
2020-03-10 21:09:15 -0300

    feat: init create title component

---
+++ src/components/Title.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const appearances = {
+  primary: 'primary',
+  secondary: 'secondary',
+  tertiary: 'tertiary',
+};
+
+const aligns = {
+  left: 'left',
+  center: 'center',
+  right: 'right',
+};
+
+const Title = styled.Text`
+  text-align: left;
+
+  ${(props) => props.type === appearances.primary && `
+    color: #000;
+  `}
+`;
+
+export default Title;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Title from '../components/Title';
+
 const Main = () => (
   <View>
-    <Text>Main</Text>
+    <Title type="primary">Main</Title>
   </View>
 );
 
