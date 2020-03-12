b228921278773ece2391230157aaba3c88594b2c
yumartins <yuri@estudioflow.com.br>
2020-03-11 21:37:33 -0300

    feat: create search

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
 
 import Container from '../components/Container';
 import Description from '../components/Description';
 import Title from '../components/Title';
+import { spacing } from '../styles/styles';
 
-const Main = () => (
-  <Container>
-    <Title text={'Welcome to \nNews upNow'} />
-    <Description text="Here you can follow all the news" />
-  </Container>
-);
+const styles = StyleSheet.create({
+  description: {
+    marginTop: spacing.xs,
+  },
+});
+
+const {
+  description,
+} = styles;
+
+const Main = () => {
+  const [search, setSearch] = useState();
+
+  return (
+    <Container>
+      <Title text={'Welcome to \nNews upNow'} />
+      <Description style={description} text="Here you can follow all the news" />
+
+      <TextInput
+        placeholder="What are you looking for?"
+        clearButtonMode="while-editing"
+        onChangeText={setSearch}
+        value={search}
+      />
+    </Container>
+  );
+};
 
 export default Main;
